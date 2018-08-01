fc0b87608f920c6bee7e32471db3dc6bbdca73e3
Mark Chavez <markchav3z@gmail.com>
2018-08-01 11:24:45 +0800

    Override request body

--- lib/bloom_rates/requests/get_quote_request.rb
+++ lib/bloom_rates/requests/get_quote_request.rb
@@ -3,9 +3,24 @@ module BloomRates
 
     include APIClientBase::Request.module(action: :post)
     attribute :token, String
+    attribute :base_currency, String
+    attribute :counter_currency, String
+    attribute :quote_type, String
+    attribute :amount, BigDecimal
 
     def path
       "/api/v1/quotes"
+    end
+
+    def body
+      {
+        quote: {
+          base_currency: base_currency,
+          counter_currency: counter_currency,
+          quote_type: quote_type,
+          amount: amount,
+        }
+      }.to_json
     end
 
     def headers
