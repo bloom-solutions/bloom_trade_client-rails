1350f38e0a4a448d43b68678928bcb3f2305eef4
Ace Subido <ace.subido@gmail.com>
2018-08-16 12:09:41 +0800

    Drop previous bloom_rates_exchange_rates table and create a bloom_trade_client_exchange_rates table

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_063218) do
+ActiveRecord::Schema.define(version: 2018_08_16_040725) do
 
-  create_table "bloom_rates_exchange_rates", force: :cascade do |t|
+  create_table "bloom_trade_client_exchange_rates", force: :cascade do |t|
     t.string "base_currency"
     t.string "counter_currency"
     t.decimal "buy"
