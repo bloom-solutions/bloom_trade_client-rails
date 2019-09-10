182bb9521d4aded146e5f9633f34329bb092052a
Ace Subido <ace.subido@gmail.com>
2019-09-10 23:33:19 +0800

    Add state to ConvertResult model

--- app/models/bloom_trade_client/convert_result.rb
+++ app/models/bloom_trade_client/convert_result.rb
@@ -3,9 +3,29 @@ module BloomTradeClient
 
     include Virtus.model
 
+    STATES = {
+      valid: "valid",
+      expired: "expired",
+      invalid: "invalid",
+    }
+
     attribute :rate, BigDecimal
     attribute :state, String
+    attribute :message, String
+    attribute :request, ConvertRequest
     attribute :expires_at, DateTime
+
+    def expired?
+      return false unless expires_at
+
+      expires_at < Time.now.utc
+    end
+
+    STATES.keys.each do |key|
+      define_method("#{key.to_s}?".to_sym) do
+        state == STATES[key.to_sym]
+      end
+    end
 
   end
 end
--- spec/models/bloom_trade_client/convert_result_spec.rb
+++ spec/models/bloom_trade_client/convert_result_spec.rb
@@ -3,7 +3,21 @@ module BloomTradeClient
 
     it { is_expected.to have_attribute(:rate, BigDecimal) }
     it { is_expected.to have_attribute(:state, String) }
+    it { is_expected.to have_attribute(:message, String) }
+    it { is_expected.to have_attribute(:request, ConvertRequest) }
     it { is_expected.to have_attribute(:expires_at, DateTime) }
+
+    described_class::STATES.keys.each do |state|
+      describe "##{state.to_s}?" do
+
+        subject do
+          result = described_class.new(state: state.to_s)
+          result.send("#{state.to_s}?".to_sym)
+        end
+
+        it { is_expected.to eq true }
+      end
+    end
 
   end
 end
