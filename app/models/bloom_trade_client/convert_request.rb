9ebe59b28edf4cd3f41de4172aadac7fba704fca
Ace Subido <ace.subido@gmail.com>
2019-09-12 18:48:09 +0800

    Add ConvertRequest#validate that raises an ArgumentError given a wrong request_type

--- app/models/bloom_trade_client/convert_request.rb
+++ app/models/bloom_trade_client/convert_request.rb
@@ -3,6 +3,8 @@ module BloomTradeClient
 
     include Virtus.model
 
+    VALID_REQUEST_TYPES = %w[buy mid sell]
+
     attribute :base_currency, String
     attribute :counter_currency, String
     attribute :reserve_currency, String, default: :default_reserve_currency
@@ -11,6 +13,16 @@ module BloomTradeClient
 
     def currency_pair
       [base_currency, counter_currency].join
+    end
+
+    def validate
+      return true if VALID_REQUEST_TYPES.include?(request_type)
+
+      error = [
+        "invalid request_type '#{request_type}'.",
+        "Valid request_types #{VALID_REQUEST_TYPES.join(", ")}",
+      ].join(" ")
+      raise ArgumentError, error
     end
 
     private
--- spec/models/bloom_trade_client/convert_request_spec.rb
+++ spec/models/bloom_trade_client/convert_request_spec.rb
@@ -7,5 +7,16 @@ module BloomTradeClient
     it { is_expected.to have_attribute(:reserve_currency, String) }
     it { is_expected.to have_attribute(:jwt, String) }
 
+    describe "#validate" do
+      it "raises an ArgumentError on invalid request type" do
+        expect {
+          described_class.new(request_type: "foo").validate
+        }.to raise_error(
+          ArgumentError,
+          "invalid request_type 'foo'. Valid request_types buy, mid, sell",
+        )
+      end
+    end
+
   end
 end
